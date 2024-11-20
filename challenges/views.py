2a9eb02120b829afb5ed532607693490f1b0b98f
janszczudlo <waterjan782@gmail.com>
2024-11-20 12:03:13 +0100

    Dynamic urls

--- challenges/views.py
+++ challenges/views.py
@@ -1,27 +1,50 @@
-from django.http import HttpResponse, HttpResponseNotFound
-
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
+from django.template.loader import render_to_string
+
+monthly_challenges = {
+    "january": 'Drink 3l of water everyday',
+    "february": 'Drink 3l of water everyday',
+    "march": '0 chicken month',
+    'april': '1 chicken',
+    'may': '12 chicken salads this month',
+    'june': '2 chicken month',
+    'july': "3 chicken",
+    'august': '4 chicken',
+    'september': '5 apples per day',
+    'october': '6 apples per day',
+    'november': '7 apples per day',
+    'december': None
+
+}
 
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse("Drink 3l of water everyday")
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    return render(request,"challenges/index.html",{
+        "months": months,
+    })
 
 
-def february(request):
-    return HttpResponse("0 chicken month")
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound('Invalid month')
+
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == 'january':
-        challenge_text = 'Drink 3l of water everyday'
-    elif month == 'february':
-        challenge_text = '0 chicken month'
-    elif month == 'march':
-        challenge_text = 'Eat at least 5000kcal a day'
-    else:
-        return HttpResponseNotFound('This month is not supported')
-
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return render(request, 'challenges/challenge.html', {
+            'text': challenge_text, 'month_name': month
+        })
+    except:
+        return HttpResponseNotFound('<h1>This month is not supported</h1>')
